01f975608e27aecb97a4e8df4a523fd457ed8c66
Sabir239 <samadinajla28@gmail.com>
2025-08-02 18:27:17 +0100

    Update py game_train.py

--- py game_train.py
+++ py game_train.py
@@ -4,7 +4,7 @@ pygame.init()
 #draw screen and title
 screen = pygame.display.set_mode((800,600))
 title = pygame.display.set_caption('uncunu')
-#import image for the soul
+#import any image you need 
 caje = pygame.image.load('Caja.png')
 #color
 black = (0,0,0)
@@ -15,6 +15,7 @@ while True:
             quit()
             exit()
     pygame.display.update()
-    #draw the caje in position:
+    #draw the image in position:
     screen.blit(caje,(200,150))
+
     clock.tick(240)
